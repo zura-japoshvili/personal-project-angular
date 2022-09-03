e77f8e3ed5ef84a5e52aad13596a953b7a3acad6
zura-japoshvili <japoshvili777@gmail.com>
2022-09-03 14:38:55 +0400

    update

--- src/app/core/interfaces/resultsInt.ts
+++ src/app/core/interfaces/resultsInt.ts
@@ -1,10 +1,10 @@
 export interface resultsInt {
-  results: [
+  results: {
     category: string,
     type: string,
     difficulty: string,
     question: string,
-    correct_answer: number | string,
+    correct_answer: string,
     incorrect_answers: []
-  ]
+  }[]
 }
--- src/app/features/game/game.component.ts
+++ src/app/features/game/game.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {GameService} from "../../core/services/game.service";
@@ -12,21 +12,51 @@ import {tap} from "rxjs";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameComponent implements OnInit {
-
   constructor(private http: HttpClient,
               private router: Router,
               private gameService: GameService,
               ) { }
 
+  @ViewChild('question') question!:ElementRef
+  @ViewChild('answersCont') answersCont!:ElementRef
+
   public settings!: gameSettingInt
+
   ngOnInit(): void {
     this.settings = this.gameService.getSelectedData()
     this.generateQuestion()
   }
+
+  private randomAnswers(arr: [], correct: string){
+      let array: string[] = arr;
+          array.push(correct)
+
+    let length = 4;
+    let randomArr = []
+    let i = 0;
+
+    while(length --){
+      i = Math.floor(Math.random() * (length +1 ));
+      randomArr.push(array[i]);
+      array.splice(i, 1);
+    }
+
+    return randomArr;
+  }
+
   public generateQuestion(){
     // this.gameService.getQuestion(this.settings.category, this.settings.diff)
     this.gameService.getQuestion(0, 0).pipe(tap((res) => {
-      console.log(res.results);
+      this.question.nativeElement.innerHTML = res.results[0].question;
+
+      const randomAnswers = this.randomAnswers(res.results[0].incorrect_answers, res.results[0].correct_answer)
+
+      let answers = '';
+      randomAnswers.forEach(value => {
+         answers+= `<button class="answers">${value}</button>`
+      })
+
+      this.answersCont.nativeElement.innerHTML = answers;
     })).subscribe()
   }
 }
